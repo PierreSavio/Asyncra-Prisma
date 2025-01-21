91fa64baf166cf3a9a833105c10161cae716ef28
Hotaru Reine <alanwalkerelease@gmail.com>
2025-01-22 06:13:03 +0900

    Prisma Asynca

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  try{
+    const { username, email, password, name, role } = req.body;
+    const user = await prisma.user.create({ data: { username, email, password, name, role } });
+    res.status(201).json(user);
+  } catch (err) {
+    console.error('Error creating user:', err);
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT} OR in reverse proxy dev.sipelikan.go.id`);
+});
